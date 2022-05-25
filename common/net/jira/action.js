b49c05c89b559fa92d4e7421d613fa33d05428b2
David Zager <david.j.zager@gmail.com>
2022-05-25 08:04:02 -0400

    major changes to add gh link to jira issue

--- common/net/jira/action.js
+++ common/net/jira/action.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+const url = require('url');
 
 class CreateJiraIssueAction {
     constructor (baseurl, project, issuetype, summary, description, labels, token) {
@@ -9,36 +10,64 @@ class CreateJiraIssueAction {
         this.description = description;
         this.labels = labels;
         this.token = token;
-      }
+    }
 
-      async execute() {
-          let config = {
-              headers: {
-                  'Authorization': `Bearer ${this.token}`,
-              }
-          }
+    async execute() {
+        let config = {
+            headers: {
+                'Authorization': `Bearer ${this.token}`,
+            }
+        }
 
-          let data = {
+        let data = {
             "fields": {
                 "summary": this.summary,
                 "description": this.description,
                 "project": {
-                  "key": this.project
+                    "key": this.project
                 },
                 "issuetype": {
-                  "name": this.issuetype
+                    "name": this.issuetype
                 },
                 "labels": this.labels,
-
             }
-          }
+        }
 
-          const response = await axios.post(`${this.baseurl}/rest/api/2/issue`, data, config);
-          //console.log('Full response:\n');
-          //console.log(response)
-          //console.log('\n')
-          return response;
-      }
+        const response = await axios.post(`${this.baseurl}/rest/api/2/issue`, data, config);
+        return response;
+    }
 }
 
-module.exports = CreateJiraIssueAction;
+class CreateJiraIssueRemoteLinkAction {
+    constructor (jiraurl, githuburl, token) {
+        this.jiraurl = jiraurl;
+        this.githuburl = githuburl;
+        this.token = token;
+
+        var q = url.parse(githuburl);
+        this.title = q.pathname.substring(1).replace('/issues/', '#')
+    }
+
+    async execute() {
+        let config = {
+            headers: {
+                'Authorization': `Bearer ${this.token}`,
+            }
+        }
+
+        let data = {
+            "object": {
+                "url": this.githuburl,
+                "title": this.title,
+                "icon": {
+                  "url16x16": "https://github.com/favicon.ico"
+                },
+            }
+        }
+
+        const response = await axios.post(`${this.jiraurl}/remotelink`, data, config);
+        return response;
+    }
+}
+
+module.exports = { CreateJiraIssueAction, CreateJiraIssueRemoteLinkAction }
--- index.js
+++ index.js
@@ -1,30 +1,43 @@
 const core = require('@actions/core')
-const CreateJiraIssueAction = require('./common/net/jira/action');
+const { CreateJiraIssueAction, CreateJiraIssueRemoteLinkAction } = require('./common/net/jira/action');
 
 async function exec () {
+  const inputs = {
+    jiraBaseUrl: core.getInput('jiraBaseUrl'),
+    project: core.getInput('project'),
+    issuetype: core.getInput('issuetype'),
+    ghIssueUrl: core.getInput('ghIssueUrl'),
+    summary: core.getInput('summary'),
+    description: core.getInput('description'),
+    labels: core.getInput('labels').split(','),
+    jiraToken: core.getInput('jiraToken'),
+  };
+
+  // create the jira issue
   try {
-    const inputs = {
-      jiraBaseUrl: core.getInput('jiraBaseUrl'),
-      project: core.getInput('project'),
-      issuetype: core.getInput('issuetype'),
-      summary: core.getInput('summary'),
-      description: core.getInput('description'),
-      labels: core.getInput('labels'),
-      jiraToken: core.getInput('jiraToken'),
-    };
-    const response = await new CreateJiraIssueAction(
+    var createResponse = await new CreateJiraIssueAction(
       inputs.jiraBaseUrl,
       inputs.project,
       inputs.issuetype,
       inputs.summary,
       inputs.description,
-      inputs.labels.split(','),
+      inputs.labels,
       inputs.jiraToken).execute();
-    console.log(response);
-    core.setOutput("issue", response.data.key);
+    console.log(createResponse);
+    core.setOutput("issue", createResponse.data.key);
   } catch (error) {
-    console.error(error)
-    process.exit(1)
+    core.setFailed(`Failed to create issue with error ${error}`)
+  }
+
+  // Add link from jira issue back to github
+  try {
+    var createLinkResponse = await new CreateJiraIssueRemoteLinkAction(
+      createResponse.data.self,
+      inputs.ghIssueUrl,
+      inputs.jiraToken).execute();
+    console.log(createLinkResponse);
+  } catch (error) {
+    core.setFailed(`Failed to create remote link with error ${error}`)
   }
 }
 
